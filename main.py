6a4830f8bf8615b886e0f4d8667af11a78f63d53
varun508 <imva508@gmail.com>
2020-03-18 00:04:36 +0530

    Create first image

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print 'hey'
+from PIL import Image, ImageDraw
+
+img = Image.open('pil_text.png')
+
+d = ImageDraw.Draw(img)
+d.text((5, 5), "Hello World", fill=(255, 0, 0))
+
+img.save('pil_text.png')
