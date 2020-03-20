b7dddfd674a22a043a2b7f12920ce8ed6b10fc06
varun508 <imva508@gmail.com>
2020-03-21 01:14:51 +0530

    separate common code

--- source/main.py
+++ source/main.py
@@ -3,3 +3,4 @@ from page2 import *
 
 
 create_page_1(1255)
+create_page_2()
--- source/page2.py
+++ source/page2.py
@@ -1,11 +1,11 @@
 from PIL import Image, ImageDraw, ImageFont
+from common import *
+
+
+img = Image.open('res/img/page_2.png')
+img_w, img_h = img.size
 
 
 def create_page_2():
-    font = ImageFont.truetype("res/font/RobotoMono-Bold.ttf", 56)
-    img = Image.open('res/img/page_2.png')
-    draw = ImageDraw.Draw(img)
-    draw.text((60, 180), "postwoman", (255, 255, 255), font=font)
-    img_w, img_h = img.size
+    render_footer(None, 'liyasthomas', img)
     img.save('generated/page_2_gen.png')
-    print(img_w, img_h)
